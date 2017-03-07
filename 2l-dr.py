b49b77e72dff62e5b22309ccd7b009d787ba2d25
shoarora <shoarora@stanford.edu>
2017-03-06 22:44:01 -0800

    argparse

--- 2l-dr.py
+++ 2l-dr.py
@@ -10,12 +10,16 @@ from model import Encoder, Decoder, Summarizer
 
 def setup_argparse():
     parser = argparse.ArgumentParser()
-    # TODO add arguments
+    parser.add_argument('--train', action='store_true')
+    parser.add_argument('--sum', action='store_true')
+    parser.add_argument('--data')
+    parser.add_argument('--input_path')
     return parser
 
 if __name__ == '__main__':
     parser = setup_argparse()
     args = parser.parse_args()
+    print args
     encoder = Encoder()
     decoder = Decoder()
     if args.train:
@@ -23,4 +27,4 @@ if __name__ == '__main__':
         summarizer.train()
     if args.sum:
         summarizer = Summarizer(encoder, decoder, args.data_path, False)
-        summarizer.summarize()
+        summarizer.summarize(args.input_path)

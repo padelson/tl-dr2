72552cbda2ad3205d44194d645c6a12b0bd737eb
shoarora <shoarora@stanford.edu>
2017-03-08 05:25:29 -0800

    shellscript

---
+++ test_data/test_vocab.py
@@ -0,0 +1,23 @@
+import collections
+
+if __name__ == '__main__':
+    n = 5500
+    vocab = collections.defaultdict(int)
+    with open('text.txt') as f:
+        for line in f:
+            for w in line.split():
+                vocab[w] += 1
+    with open('enc_vocab.txt', 'w') as f:
+        enc_vocab = sorted(vocab.items(), key=lambda x: x[1], reverse=True)[:n]
+        for word in enc_vocab:
+            f.write(word[0]+'\n')
+        f.write('<unk>')
+    with open('headlines.txt') as f:
+        for line in f:
+            for w in line.split():
+                vocab[w] += 1
+    with open('dec_vocab.txt', 'w') as f:
+        dec_vocab = sorted(vocab.items(), key=lambda x: x[1], reverse=True)[:n]
+        for word in dec_vocab:
+            f.write(word[0]+'\n')
+        f.write('<unk>')

3ffa38f1b8f0453d10d56ebd4fb3d20f35addd42
shoarora <shoarora@stanford.edu>
2017-03-09 01:19:28 -0800

    added rouge

---
+++ datatest.py
@@ -0,0 +1,22 @@
+from data import *
+import config
+import time
+from utils import *
+
+# path = './test_data'
+# b = config.BUCKETS
+# train, dev, test, enc_dict, dec_dict, num_samples = split_data(path, b)
+# enc_matrix, dec_matrix, decoder_masks, next_bucket = get_batch(train, 0, b, 64)
+# encoder_size, decoder_size = b[0]
+#
+# count = 0
+# print decoder_size
+# for step in xrange(decoder_size):
+#     print step
+#     x = decoder_masks[step]
+
+
+prog = Progbar(target=10)
+for i in range(10):
+    time.sleep(2)
+    prog.update(i+1, [('train loss', i**2)])

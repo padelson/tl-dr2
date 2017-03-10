d182e1e17908c6e66597f566c79021c513a7f42e
Jeff Hara <jhara_mac@DN51qo5m.SUNet>
2017-03-09 23:12:43 -0800

    os

--- gigaword_parse.py
+++ gigaword_parse.py
@@ -29,7 +29,7 @@ def grabContents(f, tag):
     while line != "</%s>\n" % tag:
         contents += line.replace('\n', ' ')
         line = readline(f)
-    return contents
+    return ' '.join(contents.split(' ')[:35])
 
 # assuming you have an open readable file...
 # read the <TEXT> until </TEXT>

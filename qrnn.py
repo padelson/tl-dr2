4c88f12d646fe1d780144aa530aec486dcabfeed
shoarora <shoarora@stanford.edu>
2017-03-13 04:22:57 -0700

    bug fix

--- qrnn.py
+++ qrnn.py
@@ -44,8 +44,8 @@ class QRNN(object):
                          tf.mul(1-F[:, i, :])
         return np.array(H)
 
-    def _get_filter_shape(self, vec_size):
-        return tf.pack([self.conv_size, vec_size, 1, self.num_convs*3])
+    def _get_filter_shape(self, shape):
+        return tf.pack([self.conv_size, shape[2], 1, self.num_convs*3])
 
     # convolution dimension results maths
     # out_height = ceil(float(in_height - filter_height + 1) /
@@ -59,7 +59,7 @@ class QRNN(object):
 
     def conv_layer(self, layer_id, inputs):
         with tf.variable_scope("QRNN/Variable/Convolution/"+str(layer_id)):
-            filter_shape = self._get_filter_shape(tf.shape(inputs)[2])
+            filter_shape = self._get_filter_shape(tf.shape(inputs))
             W = tf.get_variable('W', filter_shape,
                                 initializer=self.initializer)
             b = tf.get_variable('b', [self.num_convs*3],

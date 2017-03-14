12b449ac86344cad08fe843c50a1e5fdf68c9e22
shoarora <shoarora@stanford.edu>
2017-03-14 09:01:14 -0700

    tfprint

--- qrnn.py
+++ qrnn.py
@@ -86,7 +86,7 @@ class QRNN(object):
             # squeeze out 3rd D
             # split 4th (now 3rd) dim into 3
             Z, F, O = tf.split(2, 3, tf.squeeze(conv))
-            return self.fo_pool(tf.Print(tf.tanh(Z), [tf.shape(Z)]), tf.sigmoid(F), tf.sigmoid(O))
+            return self.fo_pool((tf.tanh(Z)), tf.sigmoid(F), tf.sigmoid(O))
 
     # def linear_layer(self, layer_id, inputs):
     #     # input dim [batch, seq_len, num_convs or embedding_size]
@@ -136,7 +136,7 @@ class QRNN(object):
                 #             1, num_convs*3]
                 # squeeze out 3rd D
                 # split 4th (now 3rd) dim into 3
-                Z_conv, F_conv, O_conv = tf.split(2, 3, tf.squeeze(conv))
+                Z_conv, F_conv, O_conv = tf.split(2, 3, tf.Print(tf.squeeze(conv), [tf.shape(conv)]))
                 Z = Z_conv + tf.expand_dims(Z_v, 1)
                 F = F_conv + tf.expand_dims(F_v, 1)
                 O = O_conv + tf.expand_dims(O_v, 1)

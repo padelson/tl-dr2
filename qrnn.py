8922457073759eaa502e19ec4c152b2e4f3e2c40
shoarora <shoarora@stanford.edu>
2017-03-14 07:53:05 -0700

    reshape

--- qrnn.py
+++ qrnn.py
@@ -283,4 +283,4 @@ def seq2seq_f(encoder, decoder, encoder_inputs, decoder_inputs,
             last_state = decoder.conv_with_attention(i, encode_outputs,
                                                      inputs, input_shape)
     result = tf.split(1, decoder.seq_length, last_state)
-    return tf.Print(result, [tf.shape(result)]), None
+    return tf.Print(result, [tf.shape(r) for r in result]), None

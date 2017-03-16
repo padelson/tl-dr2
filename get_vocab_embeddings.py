1d2adeb3596e4dfcdd50c140b7b5b3460196f938
shoarora <shoarora@stanford.edu>
2017-03-16 06:41:52 -0700

    done

--- get_vocab_embeddings.py
+++ get_vocab_embeddings.py
@@ -32,7 +32,7 @@ def write_results(vecs, size, vocab_name):
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 1:
+    if len(sys.argv) != 2:
         print 'provide glove vec dims you want'
         sys.exit(0)
     print 'getting encoder vocab'

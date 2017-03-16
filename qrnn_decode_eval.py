ce1c753114c753d4c39f6641f35414342ae4a453
shoarora <shoarora@stanford.edu>
2017-03-15 21:53:00 -0700

    bug

--- qrnn_decode_eval.py
+++ qrnn_decode_eval.py
@@ -1,10 +1,10 @@
 import tensorflow as tf
 
 
-def get_input_from_state(state, embeddings, output_projection):
+def get_input_from_state(state, embeddings, output_projection, state_size):
     vocab = tf.nn.xw_plus_b(state, output_projection[0], output_projection[1])
     word_ids = [tf.argmax(tf.squeeze(i)) for i
-                in tf.split(0, tf.shape(vocab)[0], state)]
+                in tf.split(0, state_size, vocab)]
     return tf.nn.embedding_lookup(embeddings, tf.arg_max(word_ids))
 
 
@@ -27,7 +27,8 @@ def decode_evaluate(decoder, encode_outputs, embedded_dec_inputs,
         else:
             step_input = layer_inputs[0]
             new_input = get_input_from_state(H[-1], embeddings,
-                                             decoder.output_projection)
+                                             decoder.output_projection,
+                                             decoder.embedding_size)
         step_input = advance_step_input(step_input, new_input)
 
         for j in range(decoder.num_layers):

a863283b3bd05cbb9fa128bda192dbb3aa9db789
shoarora <shoarora@stanford.edu>
2017-03-17 21:28:37 -0700

    reshape attention

--- rnn.py
+++ rnn.py
@@ -13,25 +13,25 @@ def seq2seq(encoder_inputs,
             output_projection=None,
             feed_previous=None):
 
-    with tf.get_variable_scope('rnn_seq2seq'):
+    with tf.variable_scope('rnn_seq2seq'):
         # encode
         encoder_cell = copy.deepcopy(cell)
         embedded_enc_input = tf.nn.embedding_lookup(embeddings, encoder_inputs)
-        encoder_outputs, encoder_state = tf.nn.dynamic_rnn(encoder_cell,
-                                                           embedded_enc_input)
+        attention_states, encoder_state = tf.nn.dynamic_rnn(encoder_cell,
+                                                            embedded_enc_input)
 
         # reshape attention states
-        top_states = [tf.reshape(e, [-1, 1, cell.output_size])
-                      for e in encoder_outputs]
-        attention_states = tf.concat(top_states, 1)
+        # top_states = [tf.reshape(e, [-1, 1, cell.output_size])
+        #               for e in encoder_outputs]
+        # attention_states = tf.concat(top_states, 1)
 
         # decode
         embedded_dec_input = tf.nn.embedding_lookup(embeddings, decoder_inputs)
         loop_function = tf.nn.seq2seq._extract_argmax_and_embed(
-                            embeddings,
-                            output_projection,
-                            True) \
-                            if feed_previous else None
+                        embeddings,
+                        output_projection,
+                        True) \
+            if feed_previous else None
 
         return tf.nn.seq2seq.attention_decoder(embedded_dec_input,
                                                encoder_state,

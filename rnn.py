170784f3a128d9986f12071fe33c06bcc8c08416
shoarora <shoarora@stanford.edu>
2017-03-17 21:47:56 -0700

    tf.cond

--- rnn.py
+++ rnn.py
@@ -18,8 +18,8 @@ def seq2seq(encoder_inputs,
         encoder_cell = copy.deepcopy(cell)
         embedded_enc_input = tf.nn.embedding_lookup(embeddings, encoder_inputs)
         attention_states, encoder_state = tf.nn.dynamic_rnn(encoder_cell,
-                                                            embedded_enc_input
-                                                            dytpe=tf.float32)
+                                                            embedded_enc_input,
+                                                            dtype=tf.float32)
 
         # reshape attention states
         # top_states = [tf.reshape(e, [-1, 1, cell.output_size])
@@ -28,14 +28,25 @@ def seq2seq(encoder_inputs,
 
         # decode
         embedded_dec_input = tf.nn.embedding_lookup(embeddings, decoder_inputs)
-        loop_function = tf.nn.seq2seq._extract_argmax_and_embed(
-                        embeddings,
-                        output_projection,
-                        True) \
-            if feed_previous else None
 
-        return tf.nn.seq2seq.attention_decoder(embedded_dec_input,
-                                               encoder_state,
-                                               attention_states,
-                                               cell,
-                                               loop_function=loop_function)
+        def decode_with_attention(feed_previous):
+            loop_function = tf.nn.seq2seq._extract_argmax_and_embed(
+                            embeddings,
+                            output_projection,
+                            True) \
+                if feed_previous else None
+
+            o, s = tf.nn.seq2seq.attention_decoder(embedded_dec_input,
+                                                   encoder_state,
+                                                   attention_states,
+                                                   cell,
+                                                   loop_function=loop_function)
+            return o+[s]
+
+        o_s = tf.cond(feed_previous, lambda: decode_with_attention(True),
+                      lambda: decode_with_attention(False))
+        output_len = len(decoder_inputs)
+        outputs = o_s[:output_len]
+        state_list = o_s[output_len:]
+        state = state_list[0]
+        return outputs, state

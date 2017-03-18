f6ddd98284bd2414efb33dc919c4c0db97c18c23
shoarora <shoarora@stanford.edu>
2017-03-17 22:57:31 -0700

    refactor

--- rnn.py
+++ rnn.py
@@ -1,5 +1,6 @@
 import copy
 
+from tensorflow.python.util import nest
 import tensorflow as tf
 
 
@@ -13,39 +14,47 @@ def seq2seq(encoder_inputs,
             output_projection=None,
             feed_previous=None):
 
-    def seq2seq_f(feed_prev):
-        with tf.variable_scope('rnn_seq2seq'):
-            # encode
-            encoder_cell = copy.deepcopy(cell)
-            embedded_enc_input = tf.nn.embedding_lookup(embeddings,
-                                                        encoder_inputs)
-            attention_states, encoder_state = tf.nn.dynamic_rnn(
-                                                encoder_cell,
-                                                embedded_enc_input,
-                                                dtype=tf.float32,
-                                                time_major=True)
+    with tf.variable_scope('seq2seq_rnn'):
+        # encode
+        encoder_cell = copy.deepcopy(cell)
+        embedded_enc_input = [tf.nn.embedding_lookup(embeddings, i)
+                              for i in encoder_inputs]
+        encoder_outputs, encoder_state = tf.nn.rnn(encoder_cell,
+                                                   embedded_enc_input,
+                                                   dtype=tf.float32)
 
-            # decode
-            embedded_dec_input = [tf.nn.embedding_lookup(embeddings, d)
-                                  for d in decoder_inputs]
-            loop_function = tf.nn.seq2seq._extract_argmax_and_embed(
-                                embeddings,
-                                output_projection,
-                                True) \
-                if feed_prev else None
-            return tf.nn.seq2seq.attention_decoder(
-                embedded_dec_input,
-                encoder_state,
-                attention_states,
-                cell,
-                loop_function=loop_function)[0]
+        # decode
+        top_states = [tf.reshape(e, [-1, 1, cell.output_size])
+                      for e in encoder_outputs]
+        attention_states = tf.concat(1, top_states)
 
-    def seq_with_previous():
-        with tf.variable_scope(tf.get_variable_scope(), reuse=None):
-            return seq2seq_f(True)
+        def decode(feed_prev_bool):
+            reuse = None if feed_prev_bool else True
+            with tf.variable_scope(tf.get_variable_scope(), reuse=reuse):
+                loop_function = tf.nn.seq2seq._extract_argmax_and_embed(
+                                    embeddings,
+                                    output_projection,
+                                    True) if feed_previous else None
+                embedded_dec_inputs = [tf.nn.embedding_lookup(embeddings, i)
+                                       for i in decoder_inputs]
+                outputs, state = tf.nn.seq2seq.attention_decoder(
+                    embedded_dec_inputs,
+                    encoder_state,
+                    attention_states,
+                    cell,
+                    loop_function=loop_function)
+                state_list = [state]
+                if nest.is_sequence(state):
+                    state_list = nest.flatten(state)
+                return outputs + state_list
 
-    def seq_without_previous():
-        with tf.variable_scope(tf.get_variable_scope(), reuse=True):
-            return seq2seq_f(False)
-
-    return tf.cond(feed_previous, seq_with_previous, seq_without_previous), None
+        outputs_and_state = tf.cond(feed_previous,
+                                    lambda: decode(True),
+                                    lambda: decode(False))
+        outputs_len = len(decoder_inputs)
+        state_list = outputs_and_state[outputs_len:]
+        state = state_list[0]
+        if nest.is_sequence(encoder_state):
+            state = nest.pack_sequence_as(structure=encoder_state,
+                                          flat_sequence=state_list)
+        return outputs_and_state[:outputs_len], state

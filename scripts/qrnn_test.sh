5df4ad5cd180b96973f7f7270705e26a8a5eab0d
shoarora <shoarora@stanford.edu>
2017-03-21 23:00:12 -0700

    fix scripts

--- scripts/qrnn_test.sh
+++ scripts/qrnn_test.sh
@@ -1,1 +1,1 @@
-python 2l-dr.py --data_path ./test_data --sess_name rnn_test --model 'qrnn' --pretrained --center_conv
+python 2l-dr.py --train --data_path ./test_data --sess_name qrnn_test --model 'qrnn' --pretrained --center_conv
